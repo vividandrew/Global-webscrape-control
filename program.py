c6c67444bb8779b779e6ac7143bd23797e2ebf60
Andrew Warnock <andrew.warnock@outlook.com>
2018-09-05 21:34:07 +0100

    Get url and classname from excel sheet

--- program.py
+++ program.py
@@ -1,13 +1,24 @@
 from bs4 import BeautifulSoup
+import openpyxl
 import requests
 
 
 def main():
-    url = input("Type url here")
-    html = get_html(requests.get(url))
-
-
-    # TODO: Create function to get list of url from excel sheet
+    tag = input("Type the tagline we are looking for div, a, p, br, h1-h5")
+    id = input("Is it a class? Y or N") == "Y"
+    
+    wb = openpyxl.load_workbook('urllist.xlsx')
+    urllist = wb.active
+    for i in range(1, 4):
+        html = get_html(requests.get(urllist['A'+str(i+1)].value))
+        classname = urllist['B'+str(i+1)].value
+        if id:
+            result = get_child_by_id(html, tag, classname)
+        else:
+            result = get_child_by_class(html, tag, classname)
+        print(result)
+        urllist['C'+str(i+1)] = str(result)
+    wb.save('urllist.xlsx')
     # TODO: Create function to save result to docx
 
     # TODO: Create function to interact with Joomla site
@@ -22,12 +33,11 @@ def get_html(html) -> BeautifulSoup:
     return BeautifulSoup(html.text, 'html.parser')
 
 
-def get_child_by_class(html, classname):
-    return html.find('div',{'class': classname})
+def get_child_by_class(html, tag, classname):
+    return html.find(tag,{'class': classname})
 
-
-def get_child_by_id(html, id):
-    return html.find('div', {'id': id})
+def get_child_by_id(html, tag, classname):
+    return html.find(tag,{'class': classname})
 
 
 if __name__ == "__main__":
